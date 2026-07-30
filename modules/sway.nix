e5f9c874f587eb9ba4577a66751bebabe54c8ba8
Kostas Andreadis <kandread@umass.edu>
2026-07-30 10:31:09 -0400

    Fixed path to swaymsg

--- modules/sway.nix
+++ modules/sway.nix
@@ -10,11 +10,13 @@
       services.swayidle = {
         enable = true;
         timeouts = [
-          { timeout = 300; command = "${pkgs.swaylock}/bin/swaylock -f"; }
+          { timeout = 300;
+            command = "${pkgs.swaylock}/bin/swaylock -f";
+          }
           {
             timeout = 600;
-            command = "swaymsg 'output * dpms off'";
-            resumeCommand = "swaymsg 'output * dpms on'";
+            command = "${pkgs.sway}/bin/swaymsg 'output * dpms off'";
+            resumeCommand = "${pkgs.sway}/bin/swaymsg 'output * dpms on'";
           }
         ];
         events = {
